08ead3109e69af7b09d4cd6f0fe61291ee117a51
Lee Ryan <2solimanleeryan@gmail.com>
2022-12-07 20:04:19 +0800

    Add header component to web app

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header>
+            <h1>Are U In A Relationship?</h1>
+            <h3>Set Relations Identifier</h3>
+        </header>
+    )
+}
+
+export default Header;
