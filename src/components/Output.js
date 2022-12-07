d4603ad18c29ae7a597058b40ce7920e1be57aa2
Lee Ryan <2solimanleeryan@gmail.com>
2022-12-07 23:12:48 +0800

    Add dysfunctional transitivity checker

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,5 +1,5 @@
 function Output() {
-    const sampleArray = [[1, 1], [2, 2], [3,3],[1,3], [3,1]];
+    const sampleArray = [[1,2], [2,3], [3,4], [2,4], [1,3]];
 
     const checkReflexive = (set) => {
         return set.every(relation => {
@@ -25,8 +25,48 @@ function Output() {
             });
         }
     }
+
+    const checkTransitive = (set) => {
+        let isTransitive = true;
+        set.map(relation => {
+            if(!hasProbability(relation, set)) {
+                return;
+            }
+            let probableRelations = getPropableRelations(relation[1], set);
+            probableRelations.map(probableRelation => {
+                if(!hasSomeTransitive([relation[0], probableRelation[1]], set)) {
+                    isTransitive = false;
+                    console.log(hasSomeTransitive([relation[0], probableRelation[1]], set));
+                    return;
+                }
+            })
+        });
+
+        function hasSomeTransitive(probableRelation, set) {
+            console.log('probable relation is ',probableRelation);
+            return set.some(relation => {
+                console.log('relation is ', relation);
+                return probableRelation[0] == relation[0] && probableRelation[1] == relation[1];
+            })
+        }
+
+        function hasProbability(relation, set) {
+            return set.some(rel => {
+                return relation[1] == rel[0];
+            });
+        }
+
+        function getPropableRelations(element, set) {
+            return set.filter(rel => {
+                return rel[0] == element;
+            })
+        }
+
+        return isTransitive;
+    }
     console.log('is reflexive? ', checkReflexive(sampleArray));
     console.log('is symmetrical? ',checkSymmetrical(sampleArray));
+    console.log('is transitive? ',checkTransitive(sampleArray));
 
     return (
         <div className="output">
