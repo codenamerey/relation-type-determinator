f4e2d0b7f66c157cb9dca7fd51d6cb4a851597cd
Lee Ryan <2solimanleeryan@gmail.com>
2022-12-07 20:58:35 +0800

    Add feature for  set reflexivity check

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,4 +1,34 @@
 function Output() {
+    const sampleArray = [[1, 1], [2, 2], [2,3], [3, 1]];
+
+    const checkReflexive = (set) => {
+        let checkedElements = [];
+        return set.every(relation => {
+             if(isChecked(relation[0], checkedElements)) return;
+             checkedElements.push(relation[0]);
+             return hasSomeReflection(relation[0], set);
+        });
+
+        function hasSomeReflection(element, relations) {
+            return relations.some(relation => {
+                return element == relation[0] && element == relation[1];
+            });
+        }
+    }
+
+    const isChecked = (element, checkedElements) => {
+        return checkedElements.find(checkedElement => {
+            return element == checkedElement;
+        });
+    }
+
+    const checkSymmetrical = () => {
+        let checkedRelations = [];
+        return 
+    }
+
+    console.log(checkReflexive(sampleArray));
+
     return (
         <div className="output">
 
