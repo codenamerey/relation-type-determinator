bd587b671334595605880c2ed288233a7485af5f
Lee Ryan <2solimanleeryan@gmail.com>
2023-02-01 11:14:27 +0800

    Add placeholder for guide

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,7 +3,7 @@ import './Input.css';
 function Input() {
     return (
         <>
-            <textarea name="setInput" id="setInput" cols="10" rows="10"></textarea>
+            <textarea name="setInput" id="setInput" cols="10" rows="10" placeholder='(1,2)(2,3)(3,4)(2,4)(1,3)'></textarea>
         </>
     )
 }
